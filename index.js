0508cae3347d1c6d536692ae6762f9975b079a69
Tony Tang <mysterise@hotmail.com>
2018-05-11 23:49:59 +1000

    fix

--- index.js
+++ index.js
@@ -14,13 +14,8 @@ var names = [];
 io.on('connection', function(socket){
 	socket.on('new user', function(session_id) {
 		// If user has no existing cookie - i.e. new user
-<<<<<<< HEAD
 		if (session_id == "") { 
 			var name = "Anon" + Math.floor((Math.random()*1000)+1);
-=======
-		if (session_id == "") {
-			var name = "Anon" + Math.floor((Math.random*1000)+1);
->>>>>>> 7cca8a8dc405038b3ab22b9c884535e782f0581c
 			while (names.indexOf(name) >= 0) {
 				name = "Anon" + Math.floor((Math.random()*1000)+1);
 			}
@@ -29,12 +24,7 @@ io.on('connection', function(socket){
 			};
 			socket.emit('setCookie', name);
 			console.log('new user:' + name + 'socket.id =' + socket.id);
-<<<<<<< HEAD
 			io.emit('chat message', 'new user joined: ' + name);
-=======
-			io.emit('new user joined: ' + name);
-
->>>>>>> 7cca8a8dc405038b3ab22b9c884535e782f0581c
 		}
 		io.emit('status', name+" has joined the chatroom");
 	});
