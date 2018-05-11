6e9eef498ec9cba0cefbf89acfb69813269fc4fe
Tony Tang <mysterise@hotmail.com>
2018-05-12 01:45:49 +1000

    f

--- static/client.js
+++ static/client.js
@@ -1,42 +1,44 @@
 
 
 $(function () {
-
+    var global_name = "";
     var socket = io();
-    if ($.cookie('user') == null || $.cookie('user') == 'AnonNaN') {
-        console.log("THERE IS NO COOKIE NIGGA");
+    if ($.cookie('name') == null || $.cookie('name') == 'AnonNaN') {
         socket.emit('new user');
     } else {
-        console.log("THERE IS A COOKIE NIGGA");
-        socket.emit('existing user', JSON.parse($.cookie('user')));
+        socket.emit('existing user', $.cookie('name'));
     }
-
+    console.log($.cookie('name'));
 
     // Submitting a post/chat
     $('form').submit(function() {
-        //console.log(JSON.stringify($.cookie('user')));
-        socket.emit('chat message', $('#m').val(), JSON.parse($.cookie('user')));
+        socket.emit('chat message', $('#m').val(), $.cookie('name'));
         $('#m').val('');
         return false;
     }); 
 
     
 
-    socket.on('chat message', function(name, message){
-        $('#messages').append(`
+    socket.on('chat message', function(name, msg){
+        var toAppend = `
             <div>
-                <li>` + name + `</li>
-                <li>` + message + `</li>
+                <div class="message_bubble white">` + name + `</div>
+                <div class="message_bubble`;
+        if (global_name == name) {
+            toAppend += " right";
+        } 
+        toAppend += `">` + msg + `</div>
             </div>
-        `);
+        `;
+
+        $('#messages').append(toAppend);
     });
 
     socket.on('status', function(msg){
-        $('#messages').append(`<li>` + msg + `</li>`);
+        $('#messages').append(msg);
     });
 
-    socket.on('setCookie', function(user){
-        console.log(JSON.stringify(user));
-        $.cookie('user', JSON.stringify(user));
+    socket.on('setCookie', function(name){
+        $.cookie('name', name);
     })
 });
