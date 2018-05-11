4c74a0d9975b95e819e855b0dd6e12dffdd7bd36
Friedpanseller <friedpanseller@gmail.com>
2018-05-11 22:36:16 +1000

    fixed

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.use('/static', express.static(__dirname + '/static'));
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -9,14 +10,17 @@ app.get('/', function(req, res){
 var users = {};
 
 io.on('connection', function(socket){
-	socket.on('new user', function(name) {
-		users[socket.id] = {
-			name: name
-		};
-		io.emit('status', name+" has joined the chatroom");
-	});
+		socket.on('new user', function(name) {
+			users[socket.id] = {
+				name: name
+			};
+			io.emit('status', name+" has joined the chatroom");
+		});
     socket.on('chat message', function(msg){
-      io.emit('chat message', msg);
+      io.emit('chat message', {
+      	name: users[socket.id].name
+      	message: msg
+      });
     });
 });
 
