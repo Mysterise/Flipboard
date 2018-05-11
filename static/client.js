948d137073b4fcbe36592ec6624a7e4dc73b893f
Ken <kenwang2227@gmail.com>
2018-05-11 23:17:34 +1000

    ..

--- static/client.js
+++ static/client.js
@@ -1,14 +1,14 @@
 $(function () {
     var socket = io();
     $('form').submit(function() {
-
         socket.emit('chat message', $('#m').val());
+        socket.emit('chat message',"5");
         $('#m').val('');
         return false;
     });
 
     socket.on('chat message', function(msg){
-        $('#messages').append('<li>' + msg + '</li>');
+        $('#messages').append('<li>' + msg + '</li>' );
     });
 
     socket.on('status', function(msg){
